d2653e17e8e07e8fba62095ff951010fdfda055b
Nick Bottomley <nhbottomley@gmail.com>
2013-06-25 18:23:16 -0400

    Functionality for add/remove

--- svgclass.js
+++ svgclass.js
@@ -1,33 +1,48 @@
 (function( $ ) {
  
-    $.fn.addSvgClass = function(c) {
+    $.fn.svgAddClass = function(c) {
         return this.each(function() {
             var currentClasses = this.attr('class').split(' ');
             var add = c.split(' ');
             for ( var i = 0; i < add.length; i++ ) {
-                
+                if ( currentClasses.indexOf(add[i]) == -1 ) {
+                    currentClasses.push(add[i]);
+                }
             }
+            this.attr('class', currentClasses.join[' ']);
         });
     };
  
-    $.fn.removeSvgClass = function(c) {
+    $.fn.svgRemoveClass = function(c) {
         return this.each(function() {
             var currentClasses = this.attr('class').split(' ');
             var remove = c.split(' ');
             for ( var i = 0; i < remove.length; i++ ) {
-            
+                if ( currentClasses.indexOf(remove[i]) != -1 ) {
+                    currentClasses.splice(currentClasses.indexOf(remove[i]), 1);
+                }
             }
+            this.attr('class', currentClasses.join[' ']);
         });
     };
     
-    $.fn.toggleSvgClass = function(c) {
+    $.fn.svgToggleClass = function(c) {
         return this.each(function() {
             var currentClasses = this.attr('class').split(' ');
             var toggle = c.split(' ');
             for ( var i = 0; i < toggle.length; i++ ) {
-                
+                if ( currentClasses.indexOf(toggle[i]) == -1 ) {
+                    currentClasses.push(toggle[i]);
+                } else {
+                    currentClasses.splice(currentClasses.indexOf(toggle[i]), 1);
+                }
             }
+            this.attr('class', currentClasses.join[' ']);
         });
+    }
+    
+    $.fn.svgSetClass = function(c) {
+        return this.attr('class', c);
     }
  
 }( jQuery ));
